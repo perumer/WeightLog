23b04fff0f8908e9bcbe1a204130648678e5cdc0
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2012-10-28 14:53:58 +0100

    format code

--- weightlog/src/main/java/de/pathmaperuma/weightlog/DataPoint.java
+++ weightlog/src/main/java/de/pathmaperuma/weightlog/DataPoint.java
@@ -6,97 +6,99 @@ import android.widget.EditText;
 
 public class DataPoint {
 
-	private float weight;
-	private float fat;
-	private float water;
-	private float muscle;
-	private int kcal;
-	private float bone;
-	private Date date;
-	
-	
-	public String getWeightString(){
-		return Float.valueOf(weight).toString();
-	}
+    private float weight;
+    private float fat;
+    private float water;
+    private float muscle;
+    private int kcal;
+    private float bone;
+    private Date date;
 
-	public String getFatString(){
-		return Float.valueOf(fat).toString();
-	}
 
-	public String getWaterString(){
-		return Float.valueOf(water).toString();
-	}
-
-	public String getMuscleString(){
-		return Float.valueOf(muscle).toString();
-	}
-
-    public String getKcalString(){
-		return Integer.valueOf(kcal).toString();
-	}
-
-	public DataPoint(String line){
-		String[] fields = line.split(";");
-		date	= new Date(Long.valueOf(fields[1]));
-		weight 	= Float.valueOf(fields[2]);
-		fat 	= Float.valueOf(fields[3]);
-		water 	= Float.valueOf(fields[4]);
-		muscle 	= Float.valueOf(fields[5]);
-		kcal 	= Integer.valueOf(fields[6]);
-		bone 	= Float.valueOf(fields[7]);
-	}
-
-	public DataPoint(float weight, float fat, float water, float muscle,
-			int kcal, float bone, Date date) {
-		super();
-		this.weight = weight;
-		this.fat = fat;
-		this.water = water;
-		this.muscle = muscle;
-		this.kcal = kcal;
-		this.bone = bone;
-		this.date = date;
-	}
-
-	public DataPoint() {
-		super();
-	}
-
-	public void setWeight(EditText et){
-		weight = Float.parseFloat(et.getText().toString());
-	}
-
-	public void setFat(EditText et){
-		fat = Float.parseFloat(et.getText().toString());
-	}
-	public void setWater(EditText et){
-		water = Float.parseFloat(et.getText().toString());
-	}
-	public void setMuscle(EditText et){
-		muscle = Float.parseFloat(et.getText().toString());
-	}
-	public void setBone(EditText et){
-		bone = Float.parseFloat(et.getText().toString());
-	}
-	
-	public void setKcal(EditText et){
-		kcal = Integer.parseInt(et.getText().toString());
-	}
-	
-	public void setDate(long dateLong){
-		date = new Date(dateLong);
-	}
-	
-    public String getNiceDateFromUnixTime(){
-        return (1900+date.getYear())+"-"+date.getMonth()+"-"+date.getDate();
+    public String getWeightString() {
+        return Float.valueOf(weight).toString();
     }
 
-    public String getUnixTimeString(){
-    	return Long.valueOf(date.getTime()).toString();
+    public String getFatString() {
+        return Float.valueOf(fat).toString();
     }
 
-	public String getBoneString() {
-		return Float.valueOf(bone).toString();
-	}
-	
+    public String getWaterString() {
+        return Float.valueOf(water).toString();
+    }
+
+    public String getMuscleString() {
+        return Float.valueOf(muscle).toString();
+    }
+
+    public String getKcalString() {
+        return Integer.valueOf(kcal).toString();
+    }
+
+    public DataPoint(String line) {
+        String[] fields = line.split(";");
+        date = new Date(Long.valueOf(fields[1]));
+        weight = Float.valueOf(fields[2]);
+        fat = Float.valueOf(fields[3]);
+        water = Float.valueOf(fields[4]);
+        muscle = Float.valueOf(fields[5]);
+        kcal = Integer.valueOf(fields[6]);
+        bone = Float.valueOf(fields[7]);
+    }
+
+    public DataPoint(float weight, float fat, float water, float muscle, int kcal, float bone, Date date) {
+        super();
+        this.weight = weight;
+        this.fat = fat;
+        this.water = water;
+        this.muscle = muscle;
+        this.kcal = kcal;
+        this.bone = bone;
+        this.date = date;
+    }
+
+    public DataPoint() {
+        super();
+    }
+
+    public void setWeight(EditText et) {
+        weight = Float.parseFloat(et.getText().toString());
+    }
+
+    public void setFat(EditText et) {
+        fat = Float.parseFloat(et.getText().toString());
+    }
+
+    public void setWater(EditText et) {
+        water = Float.parseFloat(et.getText().toString());
+    }
+
+    public void setMuscle(EditText et) {
+        muscle = Float.parseFloat(et.getText().toString());
+    }
+
+    public void setBone(EditText et) {
+        bone = Float.parseFloat(et.getText().toString());
+    }
+
+    public void setKcal(EditText et) {
+        kcal = Integer.parseInt(et.getText().toString());
+    }
+
+    public void setDate(long dateLong) {
+        date = new Date(dateLong);
+    }
+
+    public String getNiceDateFromUnixTime() {
+        return (1900 + date.getYear()) + "-" + date.getMonth() + "-" + date.getDate();
+    }
+
+    public String getUnixTimeString() {
+        return Long.valueOf(date.getTime()).toString();
+    }
+
+    public String getBoneString() {
+        return Float.valueOf(bone).toString();
+    }
+
 }
